58941ed44080cacb2f95bf30ddcb58786a33f828
Juan Pablo Rueda Barrios <jpruedaort@hotmail.com>
2021-12-20 00:00:28 -0500

    config de cors alterada

--- routes/allpost.js
+++ routes/allpost.js
@@ -4,7 +4,6 @@ const mysqlConnection = require("../src/database");
 
 //Para pedir todos los post
 router.get("/getallpost", (req, res) => {
-  console.log("por aqui pasa");
   query = `select * from all_posts `;
   mysqlConnection.query(query, [], (err, rows, fields) => {
     if (!err) {
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 //CORS
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://192.168.56.1:3001"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
